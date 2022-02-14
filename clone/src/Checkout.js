f41448895d6b7b46ce091d6126b296fc61597682
Nishant Kumar <82440294+nishant-kumar2001@users.noreply.github.com>
2022-02-14 12:40:11 +0530

    Update Checkout.js

--- clone/src/Checkout.js
+++ clone/src/Checkout.js
@@ -20,7 +20,7 @@ function Checkout() {
             {basket?.length === 0 ? (
                 <div>
                     <h2>Your Shopping Basket is empty</h2>
-                    <p>You have no items in you basket. To buy one or more items, click on "Add to basket" next to the item. </p>
+                    <p>You have no items in your basket. To buy one or more items, click on "Add to basket" next to the item. </p>
                 </div>
             ) : (  
                 // Else Part
